f4e62aa57fd6ccfdf5be7aa0fe1372645502196d
Alina Shirma <alya.sh@tut.by>
2018-05-26 17:03:31 +0300

    Fifth commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         System.out.println("Hello World !" );
 		System.out.println("Hello World from master branche!" );
 		System.out.println("Hello World from dev branche!" );
+		System.out.println("Hello World from feature branche!" );
   }
 
 }
