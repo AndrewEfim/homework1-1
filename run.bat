2184e8d820e060eb722cae0419141a93decf0f6b
Alina Shirma <alya.sh@tut.by>
2018-05-26 18:35:13 +0300

    Add bat

---
+++ run.bat
@@ -0,0 +1,4 @@
+mkdir build
+javac -d build src/HelloWorld.java
+java -cp .;build HelloWorld 
+@pause
---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !" );
+		System.out.println("Hello World from master branche!" );
+		System.out.println("Hello World from dev branche!" );
+		System.out.println("Hello World from feature branche!" );
+  }
+
+}
